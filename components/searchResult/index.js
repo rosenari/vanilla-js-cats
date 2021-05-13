832ed3850023c836a24d27b98b9d958d8d53102b
임장순 <rosenari88@gmail.com>
2021-05-13 16:43:29 +0900

    Add / #7 / Add Image Lazy loading

--- components/searchResult/index.js
+++ components/searchResult/index.js
@@ -3,6 +3,7 @@ import "./index.css";
 export default class SearchResult {
     constructor($target) {
         this._searchResult = $target;
+        this._io = null;
         this.state = [];
     }
 
@@ -10,12 +11,32 @@ export default class SearchResult {
         this._searchResult.innerHTML = '';
 
         let id = 0;
+
+        if (!this._io) this._io = this.createLazyLoadObserver();
+
         for (let item of this.state) {
             let box = document.createElement('div');
             box.classList.add('cat');
-            box.innerHTML = `<img src='${item.url}' width=250 height=250 data-id=${id++} />`;
+            box.innerHTML = `<img data-img='${item.url}' width=250 height=250 data-id=${id++} />`;
             this._searchResult.append(box);
         }
+
+        const images = document.getElementsByTagName('img');
+        Array.from(images).forEach((img) => {
+            this._io.observe(img);
+        });
+    }
+
+    createLazyLoadObserver = () => {
+        const io = new IntersectionObserver((entries, observer) => {
+            Array.from(entries).forEach((entry) => {
+                if (entry.isIntersecting) {
+                    entry.target.setAttribute('src', entry.target.dataset.img);
+                    observer.unobserve(entry.target);
+                }
+            });
+        });
+        return io;
     }
 
     setState(data) {
